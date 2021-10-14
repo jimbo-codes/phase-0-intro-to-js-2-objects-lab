59f611ddec1e4478e800bceed0b6aff027edd52b
James <jrburns96@aol.com>
2021-10-14 14:47:41 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Write your solution in this file!
+
+const employee = {
+    name: "Sam",
+    streetAddress: "street"
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+        ...employee,
+        [key]: value,
+    }
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+    }
+function deleteFromEmployeeByKey(employee, key) {
+     let newemployee = {};
+     newemployee ={...employee};
+     delete newemployee[key];
+return newemployee;
+    }
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
+/*
+  describe('destructivelyDeleteFromEmployeeByKey(employee, key)', function () {
+    it('returns employee without the deleted key/value pair', function () {
+      let newEmployee = destructivelyDeleteFromEmployeeByKey(employee, 'name');
+
+      expect(newEmployee['name']).to.equal(undefined);
+    });
+
+    it('modifies the original employee', function () {
+      let newEmployee = destructivelyDeleteFromEmployeeByKey(employee, 'name');
+
+      expect(employee['name']).to.equal(undefined);
+      expect(employee).to.equal(newEmployee);
+    });
+  });
+});
+*/
