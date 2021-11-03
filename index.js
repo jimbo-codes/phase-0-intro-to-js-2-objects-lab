6fe22246c66381cc2536fbdecceb8143fbac3c36
James Burns <jrburns96@aol.com>
2021-11-03 11:46:40 -0400

    second time round

--- index.js
+++ index.js
@@ -1,44 +1,24 @@
 // Write your solution in this file!
-
 const employee = {
-    name: "Sam",
-    streetAddress: "street"
-};
-
-function updateEmployeeWithKeyAndValue(employee, key, value) {
-    return {
-        ...employee,
-        [key]: value,
-    }
+  name: "street",
+  streetAddress: "street",
 }
-function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
-    employee[key] = value;
-    return employee;
-    }
-function deleteFromEmployeeByKey(employee, key) {
-     let newemployee = {};
-     newemployee ={...employee};
-     delete newemployee[key];
-return newemployee;
-    }
-function destructivelyDeleteFromEmployeeByKey(employee, key) {
-    delete employee[key];
-    return employee;
+updateEmployeeWithKeyAndValue = (object,key,value) => {
+  return object = {...employee,
+    [key]: value,
+  }
 }
-/*
-  describe('destructivelyDeleteFromEmployeeByKey(employee, key)', function () {
-    it('returns employee without the deleted key/value pair', function () {
-      let newEmployee = destructivelyDeleteFromEmployeeByKey(employee, 'name');
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value) {
+ employee[key] = value;
+ return employee;
+}
 
-      expect(newEmployee['name']).to.equal(undefined);
-    });
-
-    it('modifies the original employee', function () {
-      let newEmployee = destructivelyDeleteFromEmployeeByKey(employee, 'name');
-
-      expect(employee['name']).to.equal(undefined);
-      expect(employee).to.equal(newEmployee);
-    });
-  });
-});
-*/
+function deleteFromEmployeeByKey(employee,key){
+  const newobj = {...employee}
+  delete newobj[key];
+  return newobj;
+}
+function destructivelyDeleteFromEmployeeByKey(employee,key){
+  delete employee[key];
+  return employee;
+}
